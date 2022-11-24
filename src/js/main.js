59c94433eed5a4538e7af64e4ce4cc35db1f564a
ponidevito <olegmama@gmail.com>
2022-11-24 17:34:23 +0200

    master

---
+++ src/js/main.js
@@ -0,0 +1,108 @@
+// Custom scripts
+
+//burger js
+let burgerMenu = document.querySelector(".menu__icon");
+function burger() {
+  burgerMenu.classList.toggle("_active");
+  document.body.classList.toggle("_lock");
+  document.querySelector(".menu__body").classList.toggle("_active");
+}
+burgerMenu.addEventListener("click", burger);
+
+// tabs
+
+function tabs(
+  headerSelector,
+  tabSelector,
+  contentSelector,
+  activeClass,
+  display = "flex"
+) {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector);
+  function hideTabContent() {
+    content.forEach((item) => {
+      item.style.display = "none";
+    });
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  }
+  function showTabContent(i = 0) {
+    content[i].style.display = display;
+    tab[i].classList.add(activeClass);
+  }
+  hideTabContent();
+  showTabContent();
+  header.addEventListener("click", (e) => {
+    const target = e.target;
+    if (
+      target.classList.contains(tabSelector.replace(/\./, "")) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ""))
+    ) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+}
+
+
+tabs(".tabs__header", ".tabs__header-item", ".tabs__content-item", "active");
+
+//swiper
+
+const swiper = new Swiper(".swiper__testimonials", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// scroll
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    document.querySelector(".topBox").classList.add("_active");
+  } else {
+    document.querySelector(".topBox").classList.remove("_active");
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+const anchors = document.querySelectorAll("a.btn-scroll");
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
